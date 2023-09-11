6d7fc49ae972132222c094f3eae2c74294ebe317
Joshua Candia <joshuacandia74@gmail.com>
2023-09-11 19:53:58 -0300

    feat(Client/Home): home view started

---
+++ src/views/Home.tsx
@@ -0,0 +1,13 @@
+import SectionSlogan from '../components/home-sections/SectionSlogan'
+
+function Home() {
+  return (
+    <div>
+      <main>
+        <SectionSlogan />
+      </main>
+    </div>
+  )
+}
+
+export default Home
